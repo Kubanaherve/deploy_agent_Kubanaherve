a24f6a5de9b731ac8080d4e971eb3719b1dc0361
Herve <h.kubana@alustudent.com>
2026-06-13 21:42:56 +0200

    Better enhance messages

--- setup_project.sh
+++ setup_project.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "🚀 Initializing Attendance Tracker Project Factory"
+echo "Welcome! Initializing Attendance Tracker Project Factory"
 
 PROJECT_NAME=""
-read -p "Enter project suffix (e.g. student1): " PROJECT_NAME
+read -p "Enter project suffix (like ex: student1): " PROJECT_NAME
 
 BASE_DIR="attendance_tracker_${PROJECT_NAME}"
 
-echo "🔍 Checking Python3 installation..."
+echo "Currently checking Python3 installation ..."
 
 if python3 --version >/dev/null 2>&1; then
-  echo "✅ Python3 is available"
+  echo "✅ Python3 is available !"
 else
-  echo "⚠️ Warning: Python3 is not installed"
+  echo "⚠ ️ Warning: Python3 is not installe. Please run " pip install python3 " "
 fi
 
